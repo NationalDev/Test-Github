7998ac89e7f5e0dc8d11145f1d28ead3ac266fc4
NationalDev <nd.michigan@gmail.com>
2017-07-24 14:34:08 -0400

    reset

--- Scripts/Event/WTUA;BUSINESSLICENSE!~!~!APPLICATION.js
+++ Scripts/Event/WTUA;BUSINESSLICENSE!~!~!APPLICATION.js
@@ -9,99 +9,68 @@
 //WTUA:BUSINESSLICENSE/*/*/APPLICATION script
 //WTUA;BUSINESSLICENSE!~!~!APPLICATION
 
-showDebug = true, showMessage = true;
-
-
-showDebug = "true"; 
-logDebug("Start of Job" + " "); 
-mainProcess(); 
-
-function mainProcess() 
-{ 
-	capModel = aa.cap.getCapModel().getOutput(); 
-	capTypeModel = capModel.getCapType(); 
-	capTypeModel.setGroup("BusinessLicense"); 
-	capTypeModel.setType("Type"); 
-	capTypeModel.setSubType("SubType"); 
-	capTypeModel.setCategory("License"); 
-	capModel.setCapType(capTypeModel); 
-	capModel.setCapStatus("Active"); 
-	appListResult = aa.cap.getCapIDListByCapModel(capModel); 
-	logDebug("Able to retrieve app list for given model:" + appListResult.getSuccess()); 
-	if (appListResult.getSuccess()) 
-	{
-		appList = appListResult.getOutput(); 
-		for(lic in appList) 
-		{ 
-			thisCap = appList[lic]; 
-			thisCapId = aa.cap.getCapID(thisCap.getCapID().getID1(),thisCap.getCapID().getID2(),
-                                           thisCap.getCapID().getID3()).getOutput(); 
-			var b1ExpResult = aa.expiration.getLicensesByCapID(thisCapId); 
-			var b1Exp = b1ExpResult.getOutput(); 
-			var expDate = b1Exp.getExpDateString();
-			var expStat = b1Exp.getExpStatus();
-			logDebug("Attempting to use 'getExpStatus()' for " + thisCapId + " expirying on this date: " + expDate); 
-			if (expStat.equalsIgnoreCase("About to Expire")) 
-			{ 
-				if (expDate.equals("2017-12-31")) 
-				{ 
-					logDebug("Expiration Status: " + expStat); 
-					/* the following status change doesn't appear to get saved or committed*/ 
-					b1Exp.setExpStatus("Active"); 
-
-					//To save the above change, you must use the following call to commit your updates
-					aa.expiration.editB1Expiration(b1Exp.getB1Expiration());
-				}
-			}
-		} 
-	}
+if (wfStatus == "Request for Corrections") {
+        sendExternalReviewNotification();   
 }
 
-
-
-
-
-
-////*****************************************************************************************************
-//
-
-if (wfTask == "License Issuance" && wfStatus == "Issued") { 	//Status on businesslicense Application record to trigger creation of parent License record
-	//branch("LIC Establish Links to Reference Contacts"); 		// May not be needed on BusinessLicense 
-	//branch("LIC Issue Business License");						//added line 04 to this SC to getAppName from Application record and copy to parent Liceense record
-	newLic = null;
+if (wfTask == "License Issuance" && wfStatus == "Issued") {
+    newLic = null;
     newLicId = null;
     newLicIdString = null;
-    var b1ExpResult = aa.expiration.getLicensesByCapID(newLicId); 
-	var b1Exp = b1ExpResult.getOutput(); 
-	var expDate = b1Exp.getExpDateString();
-    
-    
-    
-    
-//    newLicenseType = "Business";
-//    monthsToInitialExpire = 12;
+    capName = null;
+    newLicenseType = appTypeArray[2];
+    monthsToInitialExpire = 12;
+//    newLicId = createParent(appTypeArray[0], appTypeArray[1], appTypeArray[2], "License",null);
+    // create the permit record;
+//    if (newLicId) {
+//        newLicIdString = newLicId.getCustomID();
+//        updateAppStatus("Issued","Originally Issued",newLicId);
+        
+    }
+
+}
+
+if (wfTask == "License Issuance" && wfStatus == "Issued") {
+//->branch("LIC Issue Business License");
+    newLic = null;
+    newLicId = null;
+    newLicIdString = null;
+
+    monthsToInitialExpire = 12;
     newLicId = createParent(appTypeArray[0], appTypeArray[1], appTypeArray[2], "License",null);
     // create the license record;
     if (newLicId) {
-        newLicIdString = newLicId.getCustomID();
+        
+    	newLicIdString = newLicId.getCustomID();
+        
+        copyAppSpecific(capId);
+        copyAddresses(capId,newLicId);
+        copyASITables(capId,newLicId);
+        copyASIFields(capId,newLicId);       
+        copyContacts(capId,newLicId);
+        editAppName(capName,newLicId);       
         updateAppStatus("Active","Originally Issued",newLicId);
-        //editAppName(AInfo['Doing Business As (DBA) Name'],newLicId);
-        }
-    
-    
-    
 
-//    tmpNewDate = dateAddMonths(null, monthsToInitialExpire);
+        editAppName(getAppSpecific("Doing Business As (DBA) Name"),newLicId);
+    }
 
+    
+    tmpNewDate = dateAddMonths(null, monthsToInitialExpire);
     
     if (newLicId) {
-        thisLic = new licenseObject(newLicIdString,capId);
+        thisLic = new licenseObject(newLicIdString,newLicId);
+        thisLic.setExpiration(dateAdd(tmpNewDate,0));
+
+}
+    
+    if (newLicId) {
+        thisLic = new licenseObject(newLicIdString,newLicId);
         thisLic.setExpiration(expDate);
         thisLic.setStatus("Active");
         }
 
     if (newLicId) {
-        changeCapContactTypes("Applicant","License Holder", capId);
+        changeCapContactTypes("Applicant","License Holder", newLicId);
         }
 
     if (newLicId) {
