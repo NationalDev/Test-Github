5bdc755453230c09fabf64e0a2823f58f071ff64
NationalDev <nd.michigan@gmail.com>
2017-07-26 16:15:00 -0400

    1st

--- Scripts/Event/WTUA;BUSINESSLICENSE!~!~!RENEWAL.js
+++ Scripts/Event/WTUA;BUSINESSLICENSE!~!~!RENEWAL.js
@@ -1,51 +1,80 @@
-//WTUA;BUSINESSLICENSE!~!~!RENEWAL
-//WTUA:BUSINESSLICENSE/*/*/RENEWAL (EMSE Script Name) 
-//Alternative license renewal script - use if license is not renewed automatically when payment is made
-//Business License Renewal must go through a review process before the license becomes Active for another year
-/*	showMessage = true; showDebug = true;
-	aa.runScriptInNewTransaction("WorkflowTaskUpdateAfter4Renew");
-	aa.runScript("WORKFLOWTASKUPDATEAFTER4RENEW");
-*/
-if (wfTask == "Renewal Review" && wfStatus == "Approved") {
+//*********************************************************************************************************/
+//	WTUA;BUSINESSLICENSE!~!~!RENEWAL.js															       /	
+//																			Iman Sallam @ City of Detroit  /
+//		Deploy with the script code and script title below (all caps)									   /
+//																								           /
+//					WTUA:BUSINESSLICENSE/*/*/RENEWAL							7/26/2017							 							
+//																										   /
+//*********************************************************************************************************/
+//WTUA:BUSINESSLICENSE/*/*/RENEWAL script
+
+
+if (wfStatus == "Request for Corrections") {
+        sendExternalReviewNotification();   
+}
+
+
+
+
+
+
+
+
+
+
+
+if (wfTask == "License Issuance" && wfStatus == "Renewed") {
     newLic = null;
     newLicId = null;
     newLicIdString = null;
     newLicenseType = appTypeArray[2];
-    monthsToInitialExpire = 12;
     newLicId = getParentCapID4Renewal();
-  //Update the LICENSE Record Application Status, Expiration Status, Expiration Date
+    // create the permit record;
     if (newLicId) {
         //newLicIdString = newLicId.getCustomID();
-        updateAppStatus("Issued","Originally Issued",newLicId);
-        copyAppSpecific(newLicId,"");
-        }
+        updateAppStatus("Active","Originally Issued",newLicId);
+        logDebug("newLicId =" + newLicId);
+        logDebug("capId =" + capId);
+        copyContacts(newLicId,capId);
+        copyAppSpecific(capId);
+        copyAddresses(capId,newLicId);
+        copyASITables(capId,newLicId);
+        copyLicensedProf(newLicId,capId);
+        copyASIFields(capId,newLicId);
+    }
+    var b1ExpResult = aa.expiration.getLicensesByCapID(newLicId); 
+	var b1Exp = b1ExpResult.getOutput(); 
+	var expDate = b1Exp.getExpDateString();
+	var expStat = b1Exp.getExpStatus();
 
-    tmpNewDate = dateAddMonths(null, monthsToInitialExpire);
+	tmpNewDate = new Date();
+
+    thisYear = parseInt(tmpNewDate.getYear().toString())+1900;
+
+    thisYear += 1;
+
+    Cycle= new getAppSpecific("Billing Cycle").toString();   
+    
+   var newExpDate = (Cycle + "/"+thisYear);                
+                  
+   
     if (newLicId) {
         thisLic = new licenseObject(newLicIdString,newLicId);
-        thisLic.setExpiration(dateAdd(tmpNewDate,0));
+        thisLic.setExpiration(newExpDate);
         thisLic.setStatus("Active");
         }
-	//If necessary, copy additional information to license record from renewal record
-	copyASITables(capId,newLicId);
-	}
-/*
-    if (newLicId) {
-        conToChange = null;
-        cons = aa.people.getCapContactByCapID(newLicId).getOutput();
-        for (thisCon in cons) if (cons[thisCon].getCapContactModel().getPeople().getContactType() == "Applicant") conToChange = cons[thisCon].getCapContactModel();
-        } else {
-        conToChange = null;
-        }
+            
+if (newLicId) {
+    changeCapContactTypes("Applicant","License Holder", newLicId);
+    }
 
-    if (conToChange) {
-        p = conToChange.getPeople();
-        p.setContactType("Independant Contractor");
-        conToChange.setPeople(p);
-        aa.people.editCapContact(conToChange);
-        }
+if (newLicId) {
+    copyOwner(capId, newLicId);
+    }
 
-    if (wfTask == "Permit Issuance" && wfStatus == "Issued") {
-        branch("EMSE:LicProfLookup");
-        }
-*/
+if (newLicId) {
+    copyASITables(capId,newLicId);
+    }
+logDebug("Business License Renewed" + tmpNewDate);
+}
+}
